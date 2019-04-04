ad2dc4a3492118fbcb5347cfb9f78f4c3c11a677
Cole <colehsmith38@gmail.com>
2019-04-04 16:05:39 -0400

    Create module for different instr types

---
+++ instr_types.py
@@ -0,0 +1,26 @@
+
+def j_type:
+    # JAL instruction
+    opcode = 0b110111
+
+
+def u_type():
+    # LUI, AUIPC instructions
+
+def b_type():
+    # Branch instructions
+
+def i_type():
+    # Immediate instructions
+
+def s_type():
+    # Store instructions
+
+def r_type():
+    # Register instructions
+
+def f_type():
+    # Fence instructions
+
+def c_type():
+    # CSRR instructions
